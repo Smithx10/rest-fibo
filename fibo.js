37d0327145e38aa1b8366c15e312e9f8fb4cd13c
smithx10 <bruce.smith.it@gmail.com>
2018-04-19 15:00:15 -0400

    added basic http route handling

--- fibo.js
+++ fibo.js
@@ -1,6 +1,10 @@
 var assert = require('assert-plus');
 var restify = require('restify');
+var handler = require('restify-errors');
 var bunyan = require('bunyan');
+
+var server = restify.createServer();
+
 var log = bunyan.createLogger({
     name: 'fibo',
     streams: [
@@ -16,13 +20,13 @@ var log = bunyan.createLogger({
     ]
 });
 
-
+// Create Prototype
 function fibo(opts) {
     this.log = opts.log.child({app_type: 'fibo'});
     this.log.info('Creating a Fibo');
 };
 
-
+// Get Fibonacci number from user input
 fibo.prototype.getFibonacci = function getFibonacci(num){
     assert.number(num, 'num');
     this.log.info({input: num})
@@ -40,12 +44,43 @@ fibo.prototype.getFibonacci = function getFibonacci(num){
 };
 
 log.info('start');
+
+// Create our fibo Object
 var fibo = new fibo({log: log});
 
-console.log(fibo.getFibonacci(5));
+// Create the http server
+var server = restify.createServer();
+
+// Define our Routes
+server.get('/', function (req, res, next) {
+   res.send({ hello: 'world' });
+   next();
+});
+
+server.get('/getFibonacci/:num', getFibonacciRespond);
+
+
+// Create a Simple Respond function
+function getFibonacciRespond(req, res, next) {
+    var response;
+    // Handle bad user input 
+    if (isNaN(req.params.num)) {
+       response = 'Please Specify a Number'; 
+    } else {
+        // Type Conversion
+        req.params.num = parseInt(req.params.num);
+        response = JSON.stringify(fibo.getFibonacci(req.params.num));
+    };
+
+    // Send Response.
+    res.send(response);
+  next();
+};
+
+
+    // Start the http Server on the Desired Port
+server.listen(8080, function () {
+    console.log('ready on %s', server.url);
+});
+
 log.info('done');
-
-
-
-
-
