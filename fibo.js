09b5f3005fa80ad5c66de4ec59852d45d425bdce
smithx10 <bruce.smith.it@gmail.com>
2018-04-19 13:36:21 -0400

    added base functionality

--- fibo.js
+++ fibo.js
@@ -1,20 +1,51 @@
 var assert = require('assert-plus');
-var log = require('bunyan');
 var restify = require('restify');
+var bunyan = require('bunyan');
+var log = bunyan.createLogger({
+    name: 'fibo',
+    streams: [
+        {
+            type: 'rotating-file',
+            period: '1d',
+            count: 7, 
+            path: '/home/smith/fibo.log'
+        },
+        {
+            stream: process.stdout
+        }
+    ]
+});
 
-function fibonacci(num){
 
-  var a = 1, b = 0, temp;
+function fibo(opts) {
+    this.log = opts.log.child({app_type: 'fibo'});
+    this.log.info('Creating a Fibo');
+};
 
-  while (num >= 0){
-      temp = a;
-      a = a + b;
-      b = temp;
-      num--;
+
+fibo.prototype.getFibonacci = function getFibonacci(num){
+    assert.number(num, 'num');
+    this.log.info({input: num})
+
+    var a = 1, b = 0, temp;
+
+    while (num >= 0){
+        temp = a;
+        a = a + b;
+        b = temp;
+        num--;
     };
 
-  return b;
+    return b;
 };
+
+log.info('start');
+var fibo = new fibo({log: log});
+
+console.log(fibo.getFibonacci(5));
+log.info('done');
+
+
 
 
 
