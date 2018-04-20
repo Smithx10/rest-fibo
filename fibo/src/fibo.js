8fbd1fe9937ec2d6357d3cbb0bf9edf87ca37cbe
Bruce.Smith <Bruce.Smith.IT@gmail.com>
2018-04-20 14:48:33 -0400

    Added better Logging

--- fibo/src/fibo.js
+++ fibo/src/fibo.js
@@ -6,8 +6,6 @@ var handler = require('restify-errors');
 // var manta = require('manta'); // Place holder for Shipping logs to Manta (Object Storage)
 var restify = require('restify');
 
-var server = restify.createServer();
-
 epimetheus.instrument(server);
 
 // Set Log Path from EnvVar
@@ -25,19 +23,20 @@ var log = bunyan.createLogger({
         {
             stream: process.stdout
         }
-    ]
+    ],
+    serializers: restify.bunyan.serializers,
 });
 
 // Create Prototype
 function fibo(opts) {
-    this.log = opts.log.child({app_type: 'fibo'});
+    this.log = opts.log.child({app_type: 'fibo-function'});
     this.log.info('Creating a Fibo');
 };
 
 // Get Fibonacci number from user input
 fibo.prototype.getFibonacci = function getFibonacci(num){
     assert.number(num, 'num');
-    this.log.info({input: num})
+    this.log.info({user_input: num})
 
     var a = 1, b = 0, temp;
 
@@ -48,26 +47,43 @@ fibo.prototype.getFibonacci = function getFibonacci(num){
         num--;
     };
 
+    this.log.debug({returned_value: b});
     return b;
 };
 
 log.info('starting');
 
+
+
 // Create our fibo Object
 var fibo = new fibo({log: log});
 
-
-// Define our Routes
-server.get('/', function (req, res, next) {
-   res.send({ hello: 'world' });
-   next();
+var log_restify = log.child({app_type: 'fibo-restify'});
+// Create Restify Server
+var server = restify.createServer({
+    name: 'fibo-api',
+    log: log_restify
 });
 
+// Define our Routes
+server.pre(function (req, res, next) {
+    req.log.info({req: req}, 'start');
+    next();
+});
+
+server.get('/test', function (req, res, next) {
+    req.log.info('url is "%s"', req.url);
+    res.send({ test: 'test' });
+    next();
+});
+
+// Handle getFibonacci
 server.get('/getFibonacci/:num', getFibonacciResponse);
 
 // Create a Simple Respond function
 function getFibonacciResponse(req, res, next) {
-    var response;
+    //  Log :num
+    req.log.info('Request Param is "%s"', req.params.num);
     // Handle bad user input 
     if (isNaN(req.params.num)) {
        response = 'Please Specify a Number'; 
@@ -82,7 +98,9 @@ function getFibonacciResponse(req, res, next) {
   next();
 };
 
-
+server.on('after', function (req, res, route) {
+    req.log.info({res: res}, "finished");
+});
     // Start the http Server on the Desired Port
 server.listen(8080, function () {
     console.log('ready on %s', server.url);
