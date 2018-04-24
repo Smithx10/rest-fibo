ad1c40fe01015841dc34ddbf12fa7588a6e371c2
Bruce.Smith <Bruce.Smith.IT@gmail.com>
2018-04-24 10:33:40 -0400

    fixed tests

--- fibo/src/test.js
+++ fibo/src/test.js
@@ -10,12 +10,9 @@ var client = clients.createStringClient({
     url: 'http://localhost:8080'
 });
 
-var errorObject = {
-    "code": "InvalidArgument",
-    "message": "The Parameter provided is not a Positive Number."
-} 
+var errorAPIString = 'InvalidArgumentError: The Parameter provided is not a Positive Number.';
 
-var errorString = 'The Parameter provided is not a Positive Number.' 
+var errorString = 'The Parameter provided is not a Positive Number.'; 
 
 test('getFibonacci(4): Positive Number' , function(t) {
     fibo.getFibonacci(4, function(err, data) {
@@ -61,7 +58,7 @@ test('/api/fibo/4: Positive Number', function(t) {
 test('/api/fibo/-4: Negative Number"', function(t) {
     client.get('/api/fibo/-4', function(err, req, res, data) {
         var actual = data;
-        var expected = JSON.stringify(errorObject);
+        var expected = errorAPIString;
         t.equal(actual, expected);
         console.log(actual, expected);
     });
@@ -71,7 +68,7 @@ test('/api/fibo/-4: Negative Number"', function(t) {
 test('/api/fibo/bob: String"', function(t) {
     client.get('/api/fibo/bob', function(err, req, res, data) {
         var actual = data;
-        var expected = JSON.stringify(errorObject);
+        var expected = errorAPIString;
         t.equal(actual, expected);
         console.log(actual, expected);
         server.close();
